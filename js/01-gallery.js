a2cb68031ce0c5d0d152dc8e4f38e0179313b3ff
Виктория <114413240+Viktoria2912@users.noreply.github.com>
2023-03-04 19:41:53 +0200

    gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const divBoxParentEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryMarkup = createGallaryMarkup(galleryItems);
+divBoxParentEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+divBoxParentEl.addEventListener("click", handleImageClick);
+
+function createGallaryMarkup(galleryItems) {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `
+     <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a> `;
+		})
+		.join("");
+}
+const instance = basicLightbox.create(`<img src = ""/>`, {
+	onShow: instance => {
+		window.addEventListener("keydown", closeModalbyEsc);
+	},
+	onClose: instance => {
+		window.removeEventListener("keydown", closeModalbyEsc);
+	},
+});
+
+function handleImageClick(event) {
+	event.preventDefault();
+	if (!event.target.classList.contains("gallery__image")) {
+		return;
+	}
+	instance.element().querySelector("img").src = event.target.dataset.source;
+
+	instance.show();
+}
+
+function closeModalbyEsc(event) {
+	console.log(event.key);
+	if (event.key === "Escape") {
+		instance.close();
+	}
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const divBoxParentEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryMarkup = createGallaryMarkup(galleryItems);
+divBoxParentEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+divBoxParentEl.addEventListener("click", handleImageClick);
+
+function createGallaryMarkup(galleryItems) {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" title="${description}" alt="${description}" />
+</a>`;
+		})
+		.join("");
+}
+
+function handleImageClick(event) {
+	event.preventDefault();
+
+	if (!event.target.classList.contains("gallery__image")) {
+		return;
+	}
+}
+let gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function () {});
+gallery.options.fadeSpeed = 250;
